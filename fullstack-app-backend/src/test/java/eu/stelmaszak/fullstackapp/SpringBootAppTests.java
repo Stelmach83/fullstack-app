854e9a7b35d6a34da70dd52b167ec085e5de9194
Michał Stelmaszak <stelmaszak@gmail.com>
2023-04-01 19:32:48 +0200

    backend application init

---
+++ fullstack-app-backend/src/test/java/eu/stelmaszak/fullstackapp/SpringBootAppTests.java
@@ -0,0 +1,13 @@
+package eu.stelmaszak.fullstackapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class SpringBootAppTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
